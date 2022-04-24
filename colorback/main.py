a3c7476ed29ba04777d2e77928a01c7c7eda42b7
VBoureaud <valentin.boureaud@epitech.eu>
2022-04-24 04:30:11 +0200

    ColorBack - Fix Cors issues for Front

--- colorback/main.py
+++ colorback/main.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi_utils.session import FastAPISessionMaker
 from fastapi_utils.tasks import repeat_every
+
+import json
 
 import colors
 
 app = FastAPI()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def get_colors():
-    return colors.token_color_map
+    return {
+        "pixels" : colors.token_color_map
+    }
 
 
 @app.on_event("startup")
